affb6b322ccd0c918c931c8b457721b55cceb268
tak <re795h@gmail.com>
2023-02-23 13:37:17 +0900

    update

--- 11.nft-impl/SimpleNFT.sol
+++ 11.nft-impl/SimpleNFT.sol
@@ -2,12 +2,18 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+// OpenZeppelinのNFTコントラクト
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.1/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * @title SimpleNFT
+ * @dev NFT
+ */
 contract SimpleNFT is ERC721 {
     // オーナーを保持する変数
     address public owner;
     
+    // NFT名とシンボルを定義
     string private constant tokenName = "Simple NFT";
     string private constant tokenSymbol = "SNFT";
 
@@ -17,12 +23,16 @@ contract SimpleNFT is ERC721 {
         _;
     }
 
+    // NFT名とシンボルをデプロイ時に指定
     constructor() ERC721(tokenName, tokenSymbol) {
         owner = msg.sender;
-        _mint(msg.sender, 1_000 * 10**18);
-
+        // deployerにID=1のトークンを発行する
+        _mint(owner, 1);
     }
 
+    /**
+     * @dev NFTを発行する関数
+     */
     function mint(address to, uint256 tokenId) public onlyOwner {
         _mint(to, tokenId);
     }
--- 9.erc20-impl/SimpleToken.sol
+++ 9.erc20-impl/SimpleToken.sol
@@ -2,13 +2,19 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+// OpenZeppelinのERC20コントラクト
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.1/contracts/token/ERC20/ERC20.sol";
 
-contract SimpleERC20 is ERC20 {
+/**
+ * @title SimpleERC20
+ * @dev ERC20トークン
+ */
+contract SimpleToken is ERC20 {
     // オーナーを保持する変数
     address public owner;
     
-    string private constant tokenName = "SimpleERC20";
+    // トークン名とシンボルを定義
+    string private constant tokenName = "Simple Token";
     string private constant tokenSymbol = "STK";
 
     // オーナーであるかの事前チャックを行う
@@ -17,12 +23,16 @@ contract SimpleERC20 is ERC20 {
         _;
     }
 
+    // トークン名とシンボルをデプロイ時に指定
     constructor() ERC20(tokenName, tokenSymbol) {
         owner = msg.sender;
-        _mint(msg.sender, 1_000 * 10**18);
-
+        // deployerに千枚のトークンを発行する
+        _mint(owner, 1_000 * 10**18);
     }
 
+    /**
+     * @dev トークンを発行する関数
+     */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
